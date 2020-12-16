a4f16bc2c37a328a244aed4bb97a43f36cbca9fc
dishagupta22 <61579433+dishagupta22@users.noreply.github.com>
2020-12-16 20:39:47 +0530

    Add files via upload

--- view.php
+++ view.php
@@ -1,65 +1,79 @@
 <?php
-include "connection.php";
-if(is_numeric($_GET['Policy_Num'])){    
-$sql = "select * from policy_data where Policy_Num = '".$_GET['Policy_Num']."'";    
-$result = mysqli_query($conn,$sql); 
-$row = mysqli_fetch_object($result);
-$sql1 = "select * from customer where Customer_Num = $row->Customer_Num";    
-$result1 = mysqli_query($conn,$sql1);
-$row1 = mysqli_fetch_object($result1);
-$sql2 = "select * from agent where Agent_code = '$row->Agent_code'";    
-$result2 = mysqli_query($conn,$sql2);
-$row2 = mysqli_fetch_object($result2);
-$sql3 = "select * from premium where Policy_Num = $row->Policy_Num";    
-$result3 = mysqli_query($conn,$sql3);
-$row3 = mysqli_fetch_object($result3);
-}
-
+include "input1.php";
+$f=0;
 ?>
 <html>    
     <head>    
-        <title>Policy Information</title>  
-		<link href = "style.css" type = "text/css" rel = "stylesheet" />  
+        <title>Policy Information</title>    
     </head>    
     <body>    
-        <link href = "policy/registration.css" type = "text/css" rel = "stylesheet" />
-		<ul>
-			<li><a href="policy/modified1.php"> Back to Policy datas</a></li>
-			<li style="float:right;"><a href="index.php"> Back to homepage</a></li>
-		</ul>
-		<center>
-		<h1> Policy Data </h1>
-		
-		<!-- width = "50%" border="1" cellspacing = "1" cellpadding = "3" align="center"  -->
-        <table width = "90%" height = "80%" border="0" cellspacing = "1" cellpadding = "3" align="center">
-            <tr valign = "top" align = "center">
-                <td>Policy Number: <?php echo "$row->Policy_Num";?></td>
-				<td>Agent Code: <?php echo "$row2->Agent_code";?></td>
-                <td>Customer Number: <?php echo "$row1->Customer_Num";?></td>
-				<td>Customer Name: <?php echo "$row1->First_Name $row1->Middle_Name $row1->Last_Name"; ?>				
-			</tr>
-			<!-- DOC Product Sum_Assured Pay_Period Ins_Period -->
-				<tr valign = "top" align = "center">
-					<td>DOC: <?php echo "$row->DOC";?></td>
-					<td>Product: <?php echo "$row->Product";?></td>
-					<td>Sum Assured: <?php echo "$row->Sum_Assured";?> Rs.</td>
-					<td>Payment Period: <?php echo "$row->Pay_Period";?> Yrs.</td>
-					<td>Insurance Period: <?php echo "$row->Ins_Period";?> Yrs.</td>
-				</tr>
-			<tr valign = "top" align = "center">
-				<td>Marital Status: <?php if($row1->Marital_status=='M'){echo "Married";} else{echo "UnMarried";}?></td>
-				<td>Spouse: <?php echo "$row1->Spouse";?></td>
-			</tr>
-			<tr valign = "top" align = "center">
-				<td>Address: <?php echo "$row1->Address";?></td>
-				<td>PIN: <?php echo "$row1->Pincode";?>
-				<td>Contact: <?php echo "$row1->Contact_Number";?></td>
-			</tr>
-			<tr valign = "top" align = "center">
-				<td>Mother: <?php echo "$row1->Mother_Name";?>[<?php if($row1->Mother_Status=='D'){echo "Dead";} else{echo "Alive";}?>]</td>
-				<td>Father: <?php echo "$row1->Father_Name";?>[<?php if($row1->Father_Status=='D'){echo "Dead";} else{echo "Alive";}?>]</td>
-				</tr>
-			</table>
-		</center>
+        <link href = "registration.css" type = "text/css" rel = "stylesheet" />
+        <h2>Premium</h2>    
+        <form name = "form1" action='modified.php' method = 'POST' enctype = "multipart/form-data" >    
+			<div class = "container">
+			<?php while($row = mysqli_fetch_object($result)){?>
+				<div class = "form_group">
+                    <label>Policy Number: </label><label><?php $f=1; echo $row->Policy_Num;?></label>
+				</div>
+				<div class = "form_group">
+				</div>
+				<div class = "form_group">
+					<label>Customer Number: </label><label><?php echo $row->Customer_Num;?></label>
+				</div>
+				<div class = "form_group">
+				</div>
+				<div class = "form_group">	
+					<label>Agent Code: </label><label><?php echo $row->Agent_code;?></label>
+				</div>
+				<div class = "form_group">
+				</div>
+				<div class = "form_group">	
+					<label>DOC: </label><label><?php echo $row->DOC;?></label>
+				</div>
+				<div class = "form_group">
+				</div>
+				<div class = "form_group">	
+					<label>Product: </label><label><?php echo $row->Product;?></label>
+				</div>
+				<div class = "form_group">
+				</div>
+				<div class = "form_group">	
+					<label>Sum Assured: </label><label><?php echo $row->Sum_Assured;?></label>
+				</div>
+				<div class = "form_group">
+				</div>
+				<div class = "form_group">	
+					<label>Payment Period: </label><label><?php echo $row->Pay_Period;?> years</label>
+				</div>
+				<div class = "form_group">
+				</div>
+				<div class = "form_group">	
+					<label>Insurance Period: </label><label><?php echo $row->Ins_Period;?> years</label>
+                </div>
+				<div class = "form_group">
+				<input name='Pol' value="<?php echo $row->Policy_Num;?>" hidden>
+				</div>
+				<div class = "form_group">
+				</div>				
+                <div class = "form_group">    
+                    <label>Premium payed?: </label>    
+                    <input type = "radio" name = "pay" value = "Yes" required />Yes
+					<input type = "radio" name = "pay" value = "No" required />No
+				</div>
+				<div class = "form_group">    
+                    <input type = "submit" value = "submit"/>    
+                </div>
+				<div class = "form_group">    
+                    <input type = "reset" value = "reset"/>    
+                </div>
+
+			<?php }
+			if ($f == 0){
+				echo "Policy Not Found";
+				echo "<a href='premium.php'>Back to search page</a>";
+			}?>
+				
+            </div>    
+        </form>    
     </body>    
 </html>
